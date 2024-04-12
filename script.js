73ada6bbf1eed546ab49c29fc6ef92628cd580fb
A.chitrarasu <chitrarasumit@gmail.com>
2024-04-12 14:01:09 +0530

    updated

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ const sumOfDigits = (num) => {
 console.log(sumOfDigits(123));
 
 
+// Write a recursive function to calculate the nth Fibonacci number.
 
 const fibonacci=(n,prev1,prev2)=>{
 if(n===0){
@@ -34,3 +35,34 @@ if(n===0){
 
 }
 console.log(fibonacci(5,0,1),"fib")
+
+// Write a recursive function to reverse a string
+
+const reverseTheStr=(n,str)=>{
+
+    if(n===0){
+        return ""
+    }
+ 
+return  str[n-1]+reverseTheStr(n-1,str)
+
+}
+
+let str="test"
+let n=str.length
+console.log(reverseTheStr(n,str))
+
+
+// Write a recursive function to calculate the power of a number.
+
+const powerOfNumber=(num,power)=>{
+if(power===0){
+    return 1
+}
+
+return (num)*powerOfNumber(num,power-1)
+
+}
+let num=3;
+let powerOfNum=3
+console.log(powerOfNumber(num,powerOfNum))
